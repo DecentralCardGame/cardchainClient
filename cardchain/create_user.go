11d0097b9b30ac351a93afe712e8688b428bcd8e
lxgr-linux <lxgr@protonmail.com>
2022-11-07 19:22:55 +0100

    works now

---
+++ cardchain/create_user.go
@@ -0,0 +1,37 @@
+package cardchain
+
+import (
+	"context"
+
+	"github.com/DecentralCardGame/Cardchain/x/cardchain/types"
+	"github.com/DecentralCardGame/cardchainClient/cardchain/client"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ignite/cli/ignite/pkg/cosmosclient"
+)
+
+func CreateUser(creator string, alias string, userAddressString string) (cosmosclient.Response, error) {
+	cosmos := client.Get()
+
+	address, err := cosmos.Address(creator)
+	if err != nil {
+		return cosmosclient.Response{}, err
+	}
+
+	userAddr, err := sdktypes.AccAddressFromBech32(userAddressString)
+	if err != nil {
+		return cosmosclient.Response{}, err
+	}
+
+	msg := types.NewMsgCreateuser(
+		address,
+		userAddr.String(),
+		alias,
+	)
+
+	account, err := cosmos.Account(creator)
+	if err != nil {
+		return cosmosclient.Response{}, err
+	}
+
+	return cosmos.BroadcastTx(context.Background(), account, msg)
+}
